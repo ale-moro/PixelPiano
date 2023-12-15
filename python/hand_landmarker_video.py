ab7f5f3801f6df88f4173d4ce73c3d0dd8ba55fd
mattiamassimi <mattia.massimi@mail.polimi.it>
2023-12-15 18:35:28 +0100

    Pulizia

--- python/hand_landmarker_video.py
+++ python/hand_landmarker_video.py
@@ -17,7 +17,7 @@ import numpy as np
 
 # SETUP
 MARGIN = 10  # pixels
-FONT_SIZE = 1
+FONT_SIZE = 10
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
@@ -26,43 +26,6 @@ model_path = os.path.abspath('hand_landmarker.task')
 if not os.path.exists(model_path):
     print(f"The model '{model_path}' is already downloaded.")
     wget.download('https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task', model_path)
-
-# UTIL FUNCTIONS
-def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
-
-    # Draw the hand landmarks.
-    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    hand_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      hand_landmarks_proto,
-      solutions.hands.HAND_CONNECTIONS,
-      solutions.drawing_styles.get_default_hand_landmarks_style(),
-      solutions.drawing_styles.get_default_hand_connections_style())
-
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
-
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-  return annotated_image
 
 # Download test image and show it
 # image_name = 'image.png'
@@ -154,8 +117,6 @@ def main():
 
         if cv2.waitKey(1) == ord('q'):
             break
-
-
 
     # release everything
     hand_landmarker.close()
